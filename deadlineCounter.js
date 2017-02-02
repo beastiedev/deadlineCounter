f37b9ffed47ad7b6c79e376501e7c104037e8a5a
Pavlo Bessmertny <pavlo.bessmertnyy@globallogic.com>
2017-02-02 18:24:22 +0200

    Fixed issues with time values counting; Updated README with links to demos;

--- deadlineCounter.js
+++ deadlineCounter.js
@@ -23,7 +23,8 @@ var DeadlineCounter = (function() {
      * @return {integer}  time difference in milliseconds
      */
     DeadlineCounter.prototype.getDiffTimestamp = function() {
-        return this.deadLine * 1 - (new Date()) * 1;
+        var date = new Date();
+        return this.deadLine * 1 - date * 1;
     }
 
     /**
@@ -33,23 +34,18 @@ var DeadlineCounter = (function() {
      * @return {bject}  collection with calculated time values
      */
     DeadlineCounter.prototype.calcDeadlineObject = function() {
-        var diff = this.getDiffTimestamp();
-        var sec = (diff/1000).toFixed();
-        var min = (diff/1000/60).toFixed();
-        var hour = (diff/1000/60/60).toFixed();
-        var day = (diff/1000/60/60/24).toFixed();
-        var year = (diff/1000/60/60/24/365).toFixed();
+        var diff = Math.floor(this.getDiffTimestamp()/1000);
         return {
-            totalSec: parseInt(sec),
-            totalMin: parseInt(min),
-            totalHour: parseInt(hour),
-            totalDay: parseInt(day),
-            totalYear: parseInt(year),
-            formatSec: sec%60,
-            formatMin: min%60,
-            formatHour: hour%24,
-            formatDay: day%365,
-            formatYear: parseInt(year)
+            totalSec: (diff).toFixed(),
+            totalMin: (diff/60).toFixed(),
+            totalHour: (diff/60/60).toFixed(),
+            totalDay: (diff/60/60/24).toFixed(),
+            totalYear: (diff/60/60/24/365).toFixed(),
+            formatSec: diff%60,
+            formatMin: (diff - diff%60)/60%60,
+            formatHour: Math.floor((diff - diff%60)/3600%24),
+            formatDay: Math.floor((diff - diff%60)/3600/24%365),
+            formatYear: Math.floor((diff - diff%60)/3600/24/365)
         }
     }
 
@@ -61,10 +57,12 @@ var DeadlineCounter = (function() {
      * @return {string}     Formated string
      */
     DeadlineCounter.prototype.deadlineObjectToString = function(obj) {
-        return obj.formatDay + " days "
-            + obj.formatHour + " hours "
-            + obj.formatMin + " minutes "
-            + obj.formatSec + " seconds";
+        console.log(obj)
+        return obj.formatYear + " year(s) "
+            + obj.formatDay + " day(s) "
+            + obj.formatHour + " hour(s) "
+            + obj.formatMin + " minute(s) "
+            + obj.formatSec + " second(s)";
     }
 
     /**
@@ -73,7 +71,6 @@ var DeadlineCounter = (function() {
      */
     DeadlineCounter.prototype.count = function() {
         var self = this;
-        console.log(self)
         var interval = setInterval(function() {
             self.dlLayout(self.deadlineObjectToString(self.calcDeadlineObject()));
         }, 1000);
@@ -91,7 +88,3 @@ var DeadlineCounter = (function() {
     return DeadlineCounter;
 
 })();
-
-/* create new instance of DeadlineCounter and run */
-var dl1 = new DeadlineCounter('11 Feb 2017 17:00:00', 'dl1');
-dl1.count();
